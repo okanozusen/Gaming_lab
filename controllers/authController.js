8dfb9f144cbc0bee052521c891e7d0e6a9402fdb
okan ozusen <okanozusen@hotmail.com>
2025-02-11 20:33:12 -0500

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,11 +4,12 @@ const jwt = require("jsonwebtoken");
 
 const pool = new Pool({
     user: process.env.DB_USER,
-    host: process.env.DB_HOST 
-    database: process.env.DB_NAME 
-    password: process.env.DB_PASSWORD, 
+    host: process.env.DB_HOST || "localhost",
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT || 5432,
 });
+
 
 // **Register New User**
 exports.register = async (req, res) => {
